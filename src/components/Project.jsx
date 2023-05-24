c68a8fbe0ffbb381e2587a40c2ecd6790e6856ff
Abhishek Sharma <112703149+xRaksh@users.noreply.github.com>
2023-05-24 14:14:19 +0530

    Update Project.jsx

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -7,7 +7,7 @@ export default (Project) => {
                     <p className="sm:text-lg text-gray-700 dark:text-gray-300 lg:w-11/12">Tailus prides itself not only on award-winning technology, but also on the talent of its people of some of the brightest minds and most experienced executives in business.</p>
                 </div>
                 <div className="grid gap-12 items-center md:grid-cols-3">
-                    <a href="https://xraksh.github.io/soon/" >
+                    <a href="https://xrakshecom.netlify.app/" >
                         <div className="space-y-4 text-center">
                             <img className="w-64 h-64 mx-auto object-cover rounded-xl md:w-40 md:h-40 lg:w-64 lg:h-64 glow1" src="ecommerce.gif" alt="GPT" loading="lazy" width={640} height={805} />
                             <div>
